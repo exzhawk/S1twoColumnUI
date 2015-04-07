ddd2cefa15ad19c46a0e89060e1541175b73ad05
Epix <git@exz.me>
2015-04-07 14:06:33 +0800

    fix bug

--- S1twoColumnUI.js
+++ S1twoColumnUI.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Two Column S1
 // @namespace    http://exz.me/
-// @version      0.4
+// @version      0.5
 // @description  two colomn ui for s1
 // @author       Epix
 // @match        http://bbs.saraba1st.com/2b/*
@@ -9,7 +9,7 @@
 // ==/UserScript==
 var width = 48;
 var list_on_left = true;
-if (self == top && location.href.startsWith('http://bbs.saraba1st.com/2b/forum-') || (new RegExp("mod=forumdisplay")).test(location.href)) {
+if (self==top && location.href.startsWith('http://bbs.saraba1st.com/2b/forum-') || (new RegExp("mod=forumdisplay")).test(location.href)) {
     var wp = document.querySelector('#wp');
     wp.style.width = width + '%';
     wp.style.position = 'absolute';
@@ -41,7 +41,7 @@ if (self == top && location.href.startsWith('http://bbs.saraba1st.com/2b/forum-'
         //return;
         if (target.nodeName == "TH" && target.parentNode.parentNode.parentNode.id == "threadlisttableid") {
             document.getElementById('frame').src = target.querySelector('th>a').href;
-        } else if (target.nodeName == "A" && (target.href.startsWith("thread-") || target.href.search("mod=viewthread") != -1)) {
+        } else if (target.nodeName == "A" && (target.href.search("thread-")!=-1 || target.href.search("mod=viewthread") != -1)) {
             document.getElementById('frame').src = target.href;
             event.preventDefault();
         }
